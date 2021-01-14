f1147380ff79496145c5a6b270f597cddfadaba3
JJE <elguetaj@ufm.edu>
2021-01-14 17:58:27 -0600

    functional in python?

--- clase1/ejercicios.py
+++ clase1/ejercicios.py
@@ -1,1 +1,32 @@
 print("Hello World!!")
+
+
+list = [1,2,3,4,5]
+
+def squares(plist:list):
+    return [x*x for x in list]
+
+
+def squares2(plist):
+    return [*map(lambda x: x**2, plist)]
+
+
+def gen_transformer(transformation):
+    def transformer(plist):
+        return [transformation(i) for i in plist]
+    return transformer
+
+square3 = (gen_transformer(lambda i: i**2))
+
+def gen_transform(transformation):
+    return lambda plist: [*map(transformation,plist)]
+
+cube = gen_transform(lambda i: i**3)
+
+print(squares(list))
+
+print(squares2(list))
+
+print(square3(list))
+
+print(cube(list))
